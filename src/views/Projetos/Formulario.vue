9f5b30a6bbf5d67937aed9aaec725167cb3e955a
João Paulo <joaopaulo.data@gmail.com>
2022-08-29 13:38:18 -0300

    docs: 📑  Organizando componentes com Composition API

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -20,7 +20,7 @@
 
 <script lang="ts">
 import { useStore } from '@/store';
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 import { TipoNotificacao } from '@/interfaces/INotificacao';
 
@@ -34,17 +34,17 @@ export default defineComponent({
             type: String
         }
     },
-    mounted() {
-        if(this.id) {
-            const projeto = this.store.state.projeto.projetos.find(proj => proj.id == this.id)
-            this.nomeDoProjeto = projeto?.nome || '' 
-        }
-    },
-    data(){
-        return {
-            nomeDoProjeto: ''
-        };
-    },
+    // mounted() {
+    //     if(this.id) {
+    //         const projeto = this.store.state.projeto.projetos.find(proj => proj.id == this.id)
+    //         this.nomeDoProjeto = projeto?.nome || '' 
+    //     }
+    // },
+    // data(){
+    //     return {
+    //         nomeDoProjeto: ''
+    //     };
+    // },
     methods: {
         salvar() {
             if (this.id) {
@@ -66,13 +66,22 @@ export default defineComponent({
             this.$router.push('/projetos')   
         }
     },
-    setup () {
+    setup (props) {
         const store = useStore()
         const { notificar } = useNotificador()
+
+        const nomeDoProjeto =ref("")
+        
+        if (props.id) {
+            const projeto =store.state.projeto.projetos.find(proj => proj.id == props.id)
+            nomeDoProjeto.value = projeto?.nome || '' 
+        }
+
         return {
             store,
-            notificar
-        }
+            notificar,
+            nomeDoProjeto
+        };
     }
 })
 
